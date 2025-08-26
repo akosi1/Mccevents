d40c79e84bbdfe767e2392955fc5428a598bdf13
akosi1 <bunakidboi@gmail.com>
2025-08-26 22:57:03 +0800

    Update index.php

--- public/index.php
+++ public/index.php
@@ -9,15 +9,15 @@ define('LARAVEL_START', microtime(true));
 $root = dirname(__DIR__);
 
 // Maintenance mode check
-if (file_exists($root . '/../storage/framework/maintenance.php')) {
-    require $root . '/../storage/framework/maintenance.php';
+if (file_exists($root . '/storage/framework/maintenance.php')) {
+    require $root . '/storage/framework/maintenance.php';
 }
 
 // Composer autoloader
-require $root . '/../vendor/autoload.php';
+require $root . '/vendor/autoload.php';
 
 // Bootstrap the framework
-$app = require_once $root . '/../bootstrap/app.php';
+$app = require_once $root . '/bootstrap/app.php';
 
 // Create kernel
 $kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
