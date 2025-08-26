80fa1122191c6b260ec58d67f12564aa716077af
Bry1nicks <brian2nicks@gmail.com>
2025-08-26 14:51:47 +0800

    changes

--- config/database.php
+++ config/database.php
@@ -45,7 +45,7 @@ return [
         'mysql' => [
             'driver' => 'mysql',
             'url' => env('DB_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', 'events-org.com'),
             'port' => env('DB_PORT', '3307'),
             'database' => env('DB_DATABASE', 'u802714156_events'),
             'username' => env('DB_USERNAME', 'u802714156_eventsOrgPass'),
@@ -65,7 +65,7 @@ return [
         'mariadb' => [
             'driver' => 'mariadb',
             'url' => env('DB_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', 'events-org.com'),
             'port' => env('DB_PORT', '3307'),
             'database' => env('DB_DATABASE', 'u802714156_events'),
             'username' => env('DB_USERNAME', 'u802714156_eventsOrgPass'),
