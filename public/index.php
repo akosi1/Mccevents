1808d6fa4574731cffca88e6af6a64ba36589ffb
akosi1 <bunakidboi@gmail.com>
2025-08-26 22:06:06 +0800

    Update index.php

--- public/index.php
+++ public/index.php
@@ -5,19 +5,14 @@ use Illuminate\Http\Request;
 
 define('LARAVEL_START', microtime(true));
 
-// Path to storage, vendor, bootstrap should be fixed relative to root
-$root = __DIR__;
+$root = __DIR__; // root = public_html
 
-// Check maintenance mode
 if (file_exists($root.'/storage/framework/maintenance.php')) {
     require $root.'/storage/framework/maintenance.php';
 }
 
-// Register Composer autoloader
 require $root.'/vendor/autoload.php';
 
-// Bootstrap Laravel
 $app = require_once $root.'/bootstrap/app.php';
 
-// Handle the request
 $app->handleRequest(Request::capture());
