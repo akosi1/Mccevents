31e4fa0084c4b09ed8c006bf913f56f00441e5e7
Bry1nicks <brian2nicks@gmail.com>
2025-08-27 14:11:32 +0800

    change

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,23 +1,20 @@
 <?php
 
 use Illuminate\Foundation\Application;
-use Illuminate\Foundation\Configuration\Exceptions;
-use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Http\Request;
 
-return Application::configure(basePath: dirname(__DIR__))
-    ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        commands: __DIR__.'/../routes/console.php',
-        health: '/up',
-    )
-    ->withMiddleware(function (Middleware $middleware) {
-        $middleware->web(append: [
-            \App\Http\Middleware\HandleInertiaRequests::class,
-            \Illuminate\Http\Middleware\AddLinkHeadersForPreloadedAssets::class,
-        ]);
+define('LARAVEL_START', microtime(true));
 
-        //
-    })
-    ->withExceptions(function (Exceptions $exceptions) {
-        //
-    })->create();
+// Determine if the application is in maintenance mode...
+if (file_exists($maintenance = _DIR_.'/../storage/framework/maintenance.php')) {
+    require $maintenance;
+}
+
+// Register the Composer autoloader...  
+require _DIR_.'/../vendor/autoload.php';
+
+// Bootstrap Laravel and handle the request...
+/** @var Application $app */
+$app = require_once _DIR_.'/../bootstrap/app.php';
+
+$app->handleRequest(Request::capture());
