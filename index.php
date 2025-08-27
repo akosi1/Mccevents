e46e0d9e07d0d72bd4d09c1a77c98a7bc3a02155
Bry1nicks <brian2nicks@gmail.com>
2025-08-27 11:47:53 +0800

    Change

--- index.php
+++ index.php
@@ -1,7 +1,3 @@
 <?php
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-
-// Forward all traffic to Laravel's real entry point
-require __DIR__ . '/index.php';
+header("Location: public_html/index.php");
+exit;
