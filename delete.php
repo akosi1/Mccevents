570b344456e94612b06c29afb9d7ae83bb753a3b
Bry1nicks <brian2nicks@gmail.com>
2025-08-27 09:30:21 +0800

    change

--- delete.php
+++ delete.php
@@ -1,13 +1,17 @@
-<?php
-$file = __DIR__ . '/event.sql';
+<!-- <?php
+ 
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+ //$file = __DIR__ . '/event.sql';
 
-if (file_exists($file)) {
-    if (unlink($file)) {
-        echo " File deleted successfully.";
-    } else {
-        echo " Failed to delete the file.";
-    }
-} else {
-    echo "⚠️ File does not exist.";
-}
-?>
+// if (file_exists($file)) {
+//     if (unlink($file)) {
+//         echo " File deleted successfully.";
+//     } else {
+//         echo " Failed to delete the file.";
+//     }
+// } else {
+//     echo "⚠️ File does not exist.";
+// }
+// ?> -->
