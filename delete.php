b2e5455bcb23ce64d1e59316e5884625e8809b85
Bry1nicks <brian2nicks@gmail.com>
2025-08-27 09:31:23 +0800

    cchange

--- delete.php
+++ delete.php
@@ -1,17 +1,13 @@
-<!-- <?php
- 
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
- //$file = __DIR__ . '/event.sql';
+ <?php
+ $file = __DIR__ . '/event.sql';
 
-// if (file_exists($file)) {
-//     if (unlink($file)) {
-//         echo " File deleted successfully.";
-//     } else {
-//         echo " Failed to delete the file.";
-//     }
-// } else {
-//     echo "⚠️ File does not exist.";
-// }
-// ?> -->
+if (file_exists($file)) {
+    if (unlink($file)) {
+        echo " File deleted successfully.";
+    } else {
+        echo " Failed to delete the file.";
+    }
+} else {
+    echo "⚠️\ File does not exist.";
+}
+?> 
