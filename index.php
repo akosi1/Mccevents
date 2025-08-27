cf2ab1a9faf7e4c0d4f1bcbfaebbb7b0edf8a60c
Bry1nicks <brian2nicks@gmail.com>
2025-08-27 12:02:07 +0800

    chaneg

--- index.php
+++ index.php
@@ -1,7 +1,5 @@
 <?php
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
+
 
 // Forward all traffic to Laravel's real entry point (public_html)
 require_once __DIR__ . '/index.php';
