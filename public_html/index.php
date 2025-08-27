6262416d19a8aacba6c7bd20c70e550de3812700
Bry1nicks <brian2nicks@gmail.com>
2025-08-27 12:43:36 +0800

    Change

--- public_html/index.php
+++ public_html/index.php
@@ -6,7 +6,7 @@ use Illuminate\Http\Request;
 define('LARAVEL_START', microtime(true));
 
 // Root directory is one level above public_html
-$root = __DIR__ . '/..';
+$root = dirname(__DIR__); 
 
 // Maintenance mode check
 if (file_exists($root . '/storage/framework/maintenance.php')) {
