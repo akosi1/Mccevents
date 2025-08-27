074c1f12c147f2d43f743ddcecda7dad12310e5f
Bry1nicks <brian2nicks@gmail.com>
2025-08-27 09:00:31 +0800

    c

--- import.php
+++ import.php
@@ -8,7 +8,9 @@ $dbname = 'u802714156_events';
 $username = 'u802714156_eventsOrgPass';
 $password = '1OrgEvents2025';
 
-$sqlFilePath = __DIR__ . '/event.sql'; 
+// $sqlFilePath = __DIR__ . '/event.sql'; 
+$sqlFilePath = __DIR__ . '/sql/event.sql';
+
 $conn = new mysqli($host, $username, $password, $dbname);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
