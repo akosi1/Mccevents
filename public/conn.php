251cf63cad31e638c02aa28635fdbe2e79c275d2
Bry1nicks <brian2nicks@gmail.com>
2025-08-27 16:52:37 +0800

    a

--- public/conn.php
+++ public/conn.php
@@ -3,20 +3,22 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-// database credential
-$host = "127.0.0.1:3306";
+// Database credentials
+$host = "127.0.0.1";  // No need to include port here if it's the default MySQL port
 $dbname = "u802714156_events";
 $username = "u802714156_eventsOrgPass";
 $password = "1OrgEvents2025";
 
-//create connections
-	$conn = new mysqli($host, $username, $password, $dbname);
+// Create connection
+$conn = new mysqli($host, $username, $password, $dbname);
 
-	if ($conn->connect_error) {
-	    die("Connection failed: " . $conn->connect_error);
-		//echo "Connection Faileds";
-	}
-	echo "Connection Successs";
+// Check connection
+if ($conn->connect_error) {
+    // Display connection error
+    die("Connection failed: " . $conn->connect_error);
+}
 
-	
+// If connected successfully
+echo "Connection Successful!";
+$conn->close();  // It's always a good idea to close the connection when done.
 ?>
