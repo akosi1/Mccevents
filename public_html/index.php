8a81838b8fbbb7ce2dc3fad2b9c9291fc7701185
Bry1nicks <brian2nicks@gmail.com>
2025-08-27 10:34:53 +0800

    change

--- public_html/index.php
+++ public_html/index.php
@@ -10,15 +10,15 @@ $root = __DIR__;
 
 
 // Maintenance mode check
-if (file_exists($root . '/storage/framework/maintenance.php')) {
-    require $root . '/storage/framework/maintenance.php';
+if (file_exists($root . '/../storage/framework/maintenance.php')) {
+    require $root . '/../storage/framework/maintenance.php';
 }
 
 // Composer autoloader
-require $root . '/vendor/autoload.php';
+require $root . '/../vendor/autoload.php';
 
 // Bootstrap the framework
-$app = require_once $root . '/bootstrap/app.php';
+$app = require_once $root . '/../bootstrap/app.php';
 
 // Create kernel
 $kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
