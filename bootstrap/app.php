e5c6c1a6dea5eb61cd61b3ea9dc1b2d28e372650
Bry1nicks <brian2nicks@gmail.com>
2025-08-27 14:10:28 +0800

    change

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -9,16 +9,14 @@ return Application::configure(basePath: dirname(__DIR__))
         web: __DIR__.'/../routes/web.php',
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
-        then: function () {
-            Route::middleware('web')
-                ->prefix('admin')
-                ->group(base_path('routes/admin.php'));
-        },
     )
     ->withMiddleware(function (Middleware $middleware) {
-        $middleware->alias([
-            'admin' => \App\Http\Middleware\IsAdmin::class,
+        $middleware->web(append: [
+            \App\Http\Middleware\HandleInertiaRequests::class,
+            \Illuminate\Http\Middleware\AddLinkHeadersForPreloadedAssets::class,
         ]);
+
+        //
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
