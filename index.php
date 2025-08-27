348402cb478e561019db6351b5b285076f57c2ea
Bry1nicks <brian2nicks@gmail.com>
2025-08-27 12:26:58 +0800

    chanhe

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 
-
-// Forward all traffic to Laravel's real entry point (public_html)
-require_once __DIR__ . '/index.php';
+require __DIR__ . '/public_html/index.php';
