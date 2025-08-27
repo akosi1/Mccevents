7c46f9401822955d1d82a9350bb187cf301c1949
Bry1nicks <brian2nicks@gmail.com>
2025-08-27 11:56:56 +0800

    Change

--- public_html/index.php
+++ public_html/index.php
@@ -5,7 +5,7 @@ use Illuminate\Http\Request;
 
 define('LARAVEL_START', microtime(true));
 
-// Root directory of Laravel app (one level above public_html)
+// Define root directory as parent of public_html
 $root = dirname(__DIR__);
 
 // Maintenance mode check
@@ -14,7 +14,7 @@ if (file_exists($root . '/storage/framework/maintenance.php')) {
 }
 
 // Composer autoloader
-require $root . '/../vendor/autoload.php';
+require $root . '/vendor/autoload.php';
 
 // Bootstrap Laravel
 $app = require_once $root . '/bootstrap/app.php';
