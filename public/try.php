6dc507f52c3914b419f0e1011ed89dd30966fcab
Bry1nicks <brian2nicks@gmail.com>
2025-08-27 16:06:09 +0800

    a

--- public/try.php
+++ public/try.php
@@ -1,3 +1,18 @@
 <?php
-echo "hi"
+// database credential
+$host = "127.0.0.1:3306";
+$dbname = "u802714156_events";
+$username = "u802714156_eventsOrgPass";
+$password = "1OrgEvents2025";
+
+//create connections
+	$conn = new mysqli($host, $username, $password, $dbname);
+
+	if ($conn->connect_error) {
+	    die("Connection failed: " . $conn->connect_error);
+		//echo "Connection Faileds";
+	}
+	echo "Connection Successs";
+
+	
 ?>
